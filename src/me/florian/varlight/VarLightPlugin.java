da7121189ad756c4849118ee26031870a6f563ea
Florian Schwabe <florian.schwabe@hotmail.de>
2019-03-02 01:31:29 +0100

    Bugfixes

--- src/me/florian/varlight/VarLightPlugin.java
+++ src/me/florian/varlight/VarLightPlugin.java
@@ -3,9 +3,15 @@ package me.florian.varlight;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.AnaloguePowerable;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Openable;
+import org.bukkit.block.data.Powerable;
+import org.bukkit.block.data.type.Piston;
 import org.bukkit.craftbukkit.v1_13_R2.CraftWorld;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -26,17 +32,63 @@ public class VarLightPlugin extends JavaPlugin implements Listener {
         UPDATED
     }
 
+    private boolean dependencyFound = true;
+
+    @Override
+    public void onLoad() {
+        if (Bukkit.getPluginManager().getPlugin("LightAPI") == null) {
+            dependencyFound = false;
+
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "----------------------------------------------------------------------------");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "LightAPI could not be found!");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Please be sure you downloaded and isntalled LightAPI in your plugins");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Download LightAPI at https://www.spigotmc.org/resources/lightapi-fork.48247/");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "VarLight will not load.");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "----------------------------------------------------------------------------");
+        }
+    }
+
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+        if (dependencyFound) {
+            Bukkit.getPluginManager().registerEvents(this, this);
+        } else {
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
     private boolean emitsLight(Block block) {
         return ((CraftWorld) block.getWorld()).getHandle().getChunkAt(block.getChunk().getX(), block.getChunk().getZ()).getBlockData(block.getX(), block.getY(), block.getZ()).e() > 0;
     }
 
+    private boolean isValidBlock(Block block) {
+        if (! block.getType().isBlock()) {
+            return false;
+        }
+
+        if (emitsLight(block)) {
+            return false;
+        }
+
+        BlockData blockData = block.getType().createBlockData();
+
+        if (blockData instanceof Powerable || blockData instanceof AnaloguePowerable || blockData instanceof Openable || blockData instanceof Piston) {
+            return false;
+        }
+
+        if (block.getType() == Material.SLIME_BLOCK) {
+            return false;
+        }
+
+        if (block.getType() == Material.BLUE_ICE) {
+            return true; // Packed ice is solid and occluding but blue ice isn't?
+        }
+
+        return block.getType().isSolid() && block.getType().isOccluding();
+    }
+
     private LightUpdateResult incrementLight(Block block) {
-        if (! block.getType().isBlock() || emitsLight(block)) {
+        if (! isValidBlock(block)) {
             return LightUpdateResult.INVALID_BLOCK;
         }
 
@@ -54,7 +106,7 @@ public class VarLightPlugin extends JavaPlugin implements Listener {
     }
 
     private LightUpdateResult decrementLight(Block block) {
-        if (! block.getType().isBlock() || emitsLight(block)) {
+        if (! isValidBlock(block)) {
             return LightUpdateResult.INVALID_BLOCK;
         }
 
