f1392711e3d0bbab946a030ecd4159e716aeb578
Florian <florian.schwabe@hotmail.de>
2019-11-13 14:11:02 +0100

    Start work on improved VLDB format

--- Core/src/me/shawlaf/varlight/persistence/vldb/VLDBOutputStream.java
+++ Core/src/me/shawlaf/varlight/persistence/vldb/VLDBOutputStream.java
@@ -1,7 +1,6 @@
 package me.shawlaf.varlight.persistence.vldb;
 
 import me.shawlaf.varlight.persistence.ICustomLightSource;
-import me.shawlaf.varlight.util.IntPosition;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -22,6 +21,28 @@ import java.nio.charset.StandardCharsets;
     [int16]         Length
     [Byte(Length)]  ASCII Bytes
 
+    NEW .VLDB Format:
+
+    Light Source:
+
+    [int16  ]       Relative coords in chunk (x: 0xF000, y: 0x0FF0, z: 0x000F)
+    [byte   ]       Light source data:
+                        MSB: [Nibble]   Custom light value (0 - F)
+                        LSB: [Bool]     migrated
+    [ASCII  ] Material
+
+    Chunk:
+
+    [int16  ]       Relative coords of chunk in region (x: 0xFF00, z: 0x00FF)
+    [int24  ]       Amount of light sources in chunk, length of the next array
+    [LightSource[]] Light Sources in chunk
+
+    Complete File Format:
+
+    [int16  ]       total amount of chunks with custom light sources
+    [Array  ]       Offset table:
+                [int16  ]       Relative coords of chunk in region (x: 0xFF00, z: 0x00FF)
+                [int32  ]       File offset for this chunk's Data
      */
 
 public class VLDBOutputStream implements Flushable, Closeable, AutoCloseable {
@@ -46,36 +67,64 @@ public class VLDBOutputStream implements Flushable, Closeable, AutoCloseable {
         this.baseStream.flush();
     }
 
-    public void write(ICustomLightSource[] lightSources) throws IOException {
-        writeInt(lightSources.length);
+    public void writeLightSource(ICustomLightSource lightSource) throws IOException {
+        baseStream.writeShort(
+                ((lightSource.getPosition().getX() & 0xF) << 12) |       // Encoded X
+                        ((lightSource.getPosition().getY()) << 4) |         // Encoded Y
+                        (lightSource.getPosition().getZ() & 0xF));          // Encoded Z
+        baseStream.writeByte(((lightSource.getEmittingLight() & 0xF) << 4) | (lightSource.isMigrated() ? 1 : 0));
+        writeASCII(lightSource.getType().name());
+    }
 
-        for (int i = 0; i < lightSources.length; i++) {
-            writeLightSource(lightSources[i]);
+    public void writeChunk(int chunkX, int chunkZ, ICustomLightSource[] lightSources) throws IOException {
+        baseStream.writeShort((chunkX & 0xFF) << 8 | (chunkZ & 0xFF));
+        writeInt24(lightSources.length);
+
+        for (ICustomLightSource lightSource : lightSources) {
+            writeLightSource(lightSource);
         }
     }
 
-    public void writeByte(int b) throws IOException {
-        baseStream.writeByte(b);
+    public void writeInt24(int i24) throws IOException {
+        if (i24 < 0 || i24 > ((1 << 24) - 1)) {
+            throw new IllegalArgumentException("Int24 out of range!");
+        }
+
+        baseStream.writeByte((i24 >>> 16) & 0xFF);
+        baseStream.writeByte((i24 >>> 8) & 0xFF);
+        baseStream.writeByte(i24 & 0xFF);
     }
 
-    public void writeInt(int v) throws IOException {
-        baseStream.writeInt(v);
-    }
-
-    public void writePosition(IntPosition position) throws IOException {
-        baseStream.writeLong(position.encode());
-    }
-
+    //    public void write(ICustomLightSource[] lightSources) throws IOException {
+//        writeInt(lightSources.length);
+//
+//        for (int i = 0; i < lightSources.length; i++) {
+//            writeLightSource(lightSources[i]);
+//        }
+//    }
+//
+//    public void writeByte(int b) throws IOException {
+//        baseStream.writeByte(b);
+//    }
+//
+//    public void writeInt(int v) throws IOException {
+//        baseStream.writeInt(v);
+//    }
+//
+//    public void writePosition(IntPosition position) throws IOException {
+//        baseStream.writeLong(position.encode());
+//    }
+//
     public void writeASCII(String data) throws IOException {
         byte[] asciiData = data.getBytes(StandardCharsets.US_ASCII);
 
         baseStream.writeShort(asciiData.length);
         baseStream.write(asciiData);
     }
-
-    public void writeLightSource(ICustomLightSource lightSource) throws IOException {
-        writePosition(lightSource.getPosition());
-        writeByte(((lightSource.getEmittingLight() & 0xF) << 4) | (lightSource.isMigrated() ? 1 : 0));
-        writeASCII(lightSource.getType().name());
-    }
+//
+//    public void writeLightSource(ICustomLightSource lightSource) throws IOException {
+//        writePosition(lightSource.getPosition());
+//        writeByte(((lightSource.getEmittingLight() & 0xF) << 4) | (lightSource.isMigrated() ? 1 : 0));
+//        writeASCII(lightSource.getType().name());
+//    }
 }
---
+++ Core/src/me/shawlaf/varlight/util/ChunkCoords.java
@@ -0,0 +1,27 @@
+package me.shawlaf.varlight.util;
+
+import java.util.Objects;
+
+public class ChunkCoords {
+
+    public final int x, z;
+
+    public ChunkCoords(int x, int z) {
+        this.x = x;
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChunkCoords that = (ChunkCoords) o;
+        return x == that.x &&
+                z == that.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, z);
+    }
+}
