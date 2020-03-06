ed00a988ff579c104cff269036e63165ad834cca
Florian <florian.schwabe@hotmail.de>
2020-03-06 23:07:36 +0100

    Paginate /varlight debug list

--- Core/src/me/shawlaf/varlight/spigot/command/commands/VarLightCommandDebug.java
+++ Core/src/me/shawlaf/varlight/spigot/command/commands/VarLightCommandDebug.java
@@ -9,6 +9,7 @@ import me.shawlaf.varlight.spigot.command.VarLightSubCommand;
 import me.shawlaf.varlight.spigot.persistence.WorldLightSourceManager;
 import me.shawlaf.varlight.util.ChunkCoords;
 import me.shawlaf.varlight.util.IntPosition;
+import me.shawlaf.varlight.util.Paginator;
 import me.shawlaf.varlight.util.RegionCoords;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -16,10 +17,12 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.function.ToIntFunction;
 
 import static com.mojang.brigadier.arguments.BoolArgumentType.bool;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
@@ -30,6 +33,8 @@ import static me.shawlaf.varlight.spigot.command.VarLightCommand.SUCCESS;
 
 @SuppressWarnings("DuplicatedCode")
 public class VarLightCommandDebug extends VarLightSubCommand {
+
+    private static final int PAGE_SIZE = 10;
 
     private static final RequiredArgumentBuilder<CommandSender, Integer> ARG_REGION_X = argument("regionX", integer());
     private static final RequiredArgumentBuilder<CommandSender, Integer> ARG_REGION_Z = argument("regionZ", integer());
@@ -59,22 +64,55 @@ public class VarLightCommandDebug extends VarLightSubCommand {
         return " -r|-c [regionX|chunkX] [regionZ|chunkZ]";
     }
 
+    private void suggestCoordinate(RequiredArgumentBuilder<CommandSender, Integer> coordinateArgument, ToIntFunction<Entity> coordinateSupplier) {
+        coordinateArgument.suggests(((context, builder) -> {
+            if (!(context.getSource() instanceof Entity)) {
+                return builder.buildFuture();
+            }
+
+            builder.suggest(coordinateSupplier.applyAsInt((Entity) context.getSource()));
+
+            return builder.buildFuture();
+        }));
+    }
+
     @NotNull
     @Override
     public LiteralArgumentBuilder<CommandSender> build(LiteralArgumentBuilder<CommandSender> literalArgumentBuilder) {
+
+        suggestCoordinate(ARG_CHUNK_X, e -> e.getLocation().getBlockX() >> 4);
+        suggestCoordinate(ARG_CHUNK_Z, e -> e.getLocation().getBlockZ() >> 4);
+
+        suggestCoordinate(ARG_REGION_X, e -> (e.getLocation().getBlockX() >> 4) >> 5);
+        suggestCoordinate(ARG_REGION_Z, e -> (e.getLocation().getBlockZ() >> 4) >> 5);
+
         literalArgumentBuilder.then(
                 LiteralArgumentBuilder.<CommandSender>literal("list")
                         .then(
                                 LiteralArgumentBuilder.<CommandSender>literal("-r")
                                         .executes(this::regionImplicit)
                                         .then(
-                                                ARG_REGION_X.then(ARG_REGION_Z.executes(this::regionExplicit))
+                                                ARG_REGION_X.then(ARG_REGION_Z.executes((c) -> regionExplicit(c, 1))
+                                                        .then(
+                                                                RequiredArgumentBuilder.<CommandSender, Integer>argument("rpage", integer(1))
+                                                                        .executes(
+                                                                                c -> regionExplicit(c, c.getArgument("rpage", int.class))
+                                                                        )
+                                                        )
+                                                )
                                         )
                         ).then(
                         LiteralArgumentBuilder.<CommandSender>literal("-c")
                                 .executes(this::chunkImplicit)
                                 .then(
-                                        ARG_CHUNK_X.then(ARG_CHUNK_Z.executes(this::chunkExplicit))
+                                        ARG_CHUNK_X.then(ARG_CHUNK_Z.executes(c -> chunkExplicit(c, 1))
+                                                .then(
+                                                        RequiredArgumentBuilder.<CommandSender, Integer>argument("cpage", integer(1))
+                                                                .executes(
+                                                                        c -> chunkExplicit(c, c.getArgument("cpage", int.class))
+                                                                )
+                                                )
+                                        )
                                 )
                 )
         );
@@ -122,12 +160,12 @@ public class VarLightCommandDebug extends VarLightSubCommand {
         int regionX = player.getLocation().getBlockX() >> 4 >> 5;
         int regionZ = player.getLocation().getBlockZ() >> 4 >> 5;
 
-        listLightSourcesInRegion(player, regionX, regionZ);
+        listLightSourcesInRegion(player, regionX, regionZ, 1);
 
         return SUCCESS;
     }
 
-    private int regionExplicit(CommandContext<CommandSender> context) {
+    private int regionExplicit(CommandContext<CommandSender> context, int page) {
         if (!(context.getSource() instanceof Player)) {
             failure(this, context.getSource(), "You must be a player to use this command!");
             return FAILURE;
@@ -138,7 +176,7 @@ public class VarLightCommandDebug extends VarLightSubCommand {
         int regionX = context.getArgument(ARG_REGION_X.getName(), int.class);
         int regionZ = context.getArgument(ARG_REGION_Z.getName(), int.class);
 
-        listLightSourcesInRegion(player, regionX, regionZ);
+        listLightSourcesInRegion(player, regionX, regionZ, page);
 
         return SUCCESS;
     }
@@ -154,12 +192,12 @@ public class VarLightCommandDebug extends VarLightSubCommand {
         int chunkX = player.getLocation().getBlockX() >> 4;
         int chunkZ = player.getLocation().getBlockZ() >> 4;
 
-        listLightSourcesInChunk(player, chunkX, chunkZ);
+        listLightSourcesInChunk(player, chunkX, chunkZ, 1);
 
         return SUCCESS;
     }
 
-    private int chunkExplicit(CommandContext<CommandSender> context) {
+    private int chunkExplicit(CommandContext<CommandSender> context, int page) {
         if (!(context.getSource() instanceof Player)) {
             failure(this, context.getSource(), "You must be a player to use this command!");
             return FAILURE;
@@ -170,12 +208,12 @@ public class VarLightCommandDebug extends VarLightSubCommand {
         int chunkX = context.getArgument(ARG_CHUNK_X.getName(), int.class);
         int chunkZ = context.getArgument(ARG_CHUNK_Z.getName(), int.class);
 
-        listLightSourcesInChunk(player, chunkX, chunkZ);
+        listLightSourcesInChunk(player, chunkX, chunkZ, page);
 
         return SUCCESS;
     }
 
-    private void listLightSourcesInRegion(Player player, int regionX, int regionZ) {
+    private void listLightSourcesInRegion(Player player, int regionX, int regionZ, int page) {
         WorldLightSourceManager manager = plugin.getManager(player.getWorld());
 
         if (manager == null) {
@@ -185,11 +223,16 @@ public class VarLightCommandDebug extends VarLightSubCommand {
 
         List<IntPosition> all = manager.getNLSFile(new RegionCoords(regionX, regionZ)).getAllLightSources();
 
-        player.sendMessage(String.format("Light sources in region (%d | %d): [%d]", regionX, regionZ, all.size()));
-        listInternal(player, all);
+        int pages = Paginator.getAmountPages(all.size(), PAGE_SIZE);
+        page = Math.min(page, pages);
+
+        List<IntPosition> pageList = Paginator.paginateEntries(all, PAGE_SIZE, page);
+
+        player.sendMessage(String.format("Light sources in region (%d | %d): %d (Showing Page %d / %d)", regionX, regionZ, all.size(), page, pages));
+        listInternal(player, pageList);
     }
 
-    private void listLightSourcesInChunk(Player player, int chunkX, int chunkZ) {
+    private void listLightSourcesInChunk(Player player, int chunkX, int chunkZ, int page) {
         WorldLightSourceManager manager = plugin.getManager(player.getWorld());
 
         if (manager == null) {
@@ -201,8 +244,13 @@ public class VarLightCommandDebug extends VarLightSubCommand {
 
         List<IntPosition> all = manager.getNLSFile(chunkCoords.toRegionCoords()).getAllLightSources(chunkCoords);
 
-        player.sendMessage(String.format("Light sources in chunk (%d | %d): [%d]", chunkX, chunkZ, all.size()));
-        listInternal(player, all);
+        int pages = Paginator.getAmountPages(all.size(), PAGE_SIZE);
+        page = Math.min(page, pages);
+
+        List<IntPosition> pageList = Paginator.paginateEntries(all, PAGE_SIZE, page);
+
+        player.sendMessage(String.format("Light sources in chunk (%d | %d): %d (Showing Page %d / %d)", chunkX, chunkZ, all.size(), page, pages));
+        listInternal(player, pageList);
     }
 
     @SuppressWarnings("ConstantConditions")
