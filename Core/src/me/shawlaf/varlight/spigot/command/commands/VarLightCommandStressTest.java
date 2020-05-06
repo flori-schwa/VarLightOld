76f0c98a90bf7e92df7d73accb7f4ac9ba1d3963
Florian <florian.schwabe@hotmail.de>
2020-05-06 14:14:26 +0200

    Test multiple ls at once

--- Core/src/me/shawlaf/varlight/spigot/command/commands/VarLightCommandStressTest.java
+++ Core/src/me/shawlaf/varlight/spigot/command/commands/VarLightCommandStressTest.java
@@ -3,6 +3,7 @@ package me.shawlaf.varlight.spigot.command.commands;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import me.shawlaf.varlight.spigot.VarLightPlugin;
+import me.shawlaf.varlight.spigot.command.VarLightCommand;
 import me.shawlaf.varlight.spigot.command.VarLightSubCommand;
 import me.shawlaf.varlight.spigot.persistence.WorldLightSourceManager;
 import me.shawlaf.varlight.spigot.util.IntPositionExtension;
@@ -15,6 +16,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static me.shawlaf.command.result.CommandResult.failure;
@@ -36,15 +38,25 @@ public class VarLightCommandStressTest extends VarLightSubCommand {
 
         node.then(
                 LiteralArgumentBuilder.<CommandSender>literal("start")
-                    .executes(this::runStart)
+                        .executes(this::runStart)
         );
 
         node.then(
                 LiteralArgumentBuilder.<CommandSender>literal("stop")
-                    .executes(this::runStop)
+                        .executes(this::runStop)
         );
 
         return node;
+    }
+
+    private static <T> boolean containsArray(T[] array, T element) {
+        for (T t : array) {
+            if (Objects.equals(t, element)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private int runStart(CommandContext<CommandSender> context) {
@@ -71,17 +83,22 @@ public class VarLightCommandStressTest extends VarLightSubCommand {
                 }
 
                 IntPosition center = IntPositionExtension.toIntPosition(player.getLocation());
+
+                final IntPosition[] toPlace = new IntPosition[ThreadLocalRandom.current().nextInt(1, 16)];
+
                 IntPosition tmp;
 
-                do {
-                    int dx = (ThreadLocalRandom.current().nextBoolean() ? -1 : 1) * ThreadLocalRandom.current().nextInt(100);
-                    int dz = (ThreadLocalRandom.current().nextBoolean() ? -1 : 1) * ThreadLocalRandom.current().nextInt(100);
-                    int dy = (ThreadLocalRandom.current().nextBoolean() ? -1 : 1) * ThreadLocalRandom.current().nextInt(100);
+                for (int i = 0; i < toPlace.length; i++) {
+                    do {
+                        int dx = (ThreadLocalRandom.current().nextBoolean() ? -1 : 1) * ThreadLocalRandom.current().nextInt(100);
+                        int dz = (ThreadLocalRandom.current().nextBoolean() ? -1 : 1) * ThreadLocalRandom.current().nextInt(100);
+                        int dy = (ThreadLocalRandom.current().nextBoolean() ? -1 : 1) * ThreadLocalRandom.current().nextInt(100);
 
-                    tmp = center.getRelative(dx, -center.y + dy, dz);
-                } while (plugin.getNmsAdapter().isIllegalBlock(IntPositionExtension.toBlock(tmp, player.getWorld())) || manager.getCustomLuminance(tmp, 0) > 0);
+                        tmp = center.getRelative(dx, -center.y + dy, dz);
+                    } while ((plugin.getNmsAdapter().isIllegalBlock(IntPositionExtension.toBlock(tmp, player.getWorld())) || manager.getCustomLuminance(tmp, 0) > 0) && !containsArray(toPlace, tmp));
 
-                final IntPosition toPlace = tmp;
+                    toPlace[i] = tmp;
+                }
 
                 Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
                     try {
@@ -90,8 +107,10 @@ public class VarLightCommandStressTest extends VarLightSubCommand {
                         // Ignore
                     }
 
-                    if (LightSourceUtil.placeNewLightSource(plugin, IntPositionExtension.toLocation(toPlace, world), 15).successful()) {
-                        player.sendMessage("[Stress Test] Placed LS at " + toPlace.toShortString());
+                    for (IntPosition position : toPlace) {
+                        if (LightSourceUtil.placeNewLightSource(plugin, IntPositionExtension.toLocation(position, world), 15).successful()) {
+                            player.sendMessage("[Stress Test] Placed LS at " + position.toShortString());
+                        }
                     }
                 });
             }, 1L, 1L);
