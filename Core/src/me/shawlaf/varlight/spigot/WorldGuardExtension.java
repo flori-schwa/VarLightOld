f6c2c994b3f6e1c8cf6fa77afcc0340977013af6
Florian Schwabe <florian.schwabe@hotmail.de>
2020-05-09 18:40:45 +0200

    Bugfix

--- Core/src/me/shawlaf/varlight/spigot/WorldGuardExtension.java
+++ Core/src/me/shawlaf/varlight/spigot/WorldGuardExtension.java
@@ -5,10 +5,11 @@ import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.commands.CommandUtils;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import me.shawlaf.varlight.spigot.event.LightUpdateEvent;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -32,11 +33,26 @@ public class WorldGuardExtension implements Listener {
 
         Player source = (Player) lightUpdateEvent.getSource();
 
-        if (!canModifyAt(source, lightUpdateEvent.toLocation())) {
+        World weWorld = BukkitAdapter.adapt(lightUpdateEvent.getWorld());
 
-            source.sendMessage(ChatColor.RED + "You cannot edit in this area");
+        LocalPlayer wrappedPlayer = worldGuardPlugin.wrapPlayer(source);
 
-            lightUpdateEvent.setCancelled(true);
+        boolean canByPass = worldGuard.getPlatform().getSessionManager().hasBypass(wrappedPlayer, weWorld);
+
+        if (!canByPass) {
+            RegionQuery regionQuery = worldGuard.getPlatform().getRegionContainer().createQuery();
+            com.sk89q.worldedit.util.Location weLocation = BukkitAdapter.adapt(lightUpdateEvent.toLocation());
+            ApplicableRegionSet lightModifyAt = regionQuery.getApplicableRegions(weLocation);
+
+            if (!lightModifyAt.testState(wrappedPlayer, Flags.BUILD)) {
+                lightUpdateEvent.setCancelled(true);
+
+                String message = lightModifyAt.queryValue(wrappedPlayer, Flags.DENY_MESSAGE);
+                message = worldGuard.getPlatform().getMatcher().replaceMacros(wrappedPlayer, message);
+                message = CommandUtils.replaceColorMacros(message);
+
+                wrappedPlayer.printRaw(message.replace("%what%", "edit custom Light sources"));
+            }
         }
     }
 
@@ -55,7 +71,7 @@ public class WorldGuardExtension implements Listener {
 
         com.sk89q.worldedit.util.Location weLocation = BukkitAdapter.adapt(at);
 
-        return regionQuery.testState(weLocation, wrappedPlayer, Flags.INTERACT);
+        return regionQuery.testState(weLocation, wrappedPlayer, Flags.BUILD);
     }
 
 }
